22958e9273d940f432345126206e6d9d8c6ad7e0
Ryan Walsh <ryanwalsh@Ryans-MacBook-Pro.local>
2015-08-29 17:53:33 -0600

    Angular feels like cheating

--- FriendController.js
+++ FriendController.js
@@ -1,4 +1,18 @@
+var app = angular.module('friendsList');
 
+app.controller('FriendController', function($scope, $http) {
+
+	$scope.friends = [];
+
+	$http({
+		method: 'GET',
+		url: 'friend-data.json'
+	})
+	.success(function(data) {
+		$scope.friends = data;
+	});
+
+});
 
 
 
---
+++ app.js
@@ -0,0 +1,5 @@
+var app = angular.module('friendsList', []);
+
+app.controller('FriendController', function($scope) {
+	
+});
